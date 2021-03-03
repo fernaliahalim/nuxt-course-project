933717ded305ab6d113861a64f8eac9c81b7bfd0
Fernalia <fernalia.h@gmail.com>
2021-03-04 02:52:44 +0700

    Section 3: Project - Pages, Routing, & views (until preparung the backend)

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class="post-list">
+        <PostPreview 
+            id="1"
+            :is-admin="isAdmin"
+            thumbnail="https://specials-images.forbesimg.com/imageserve/5f302109ffad89f9130e07db/960x0.jpg?cropX1=0&cropX2=4800&cropY1=243&cropY2=2943"
+            title="Hello there!"
+            previewText="This is my first post!"/>
+        <PostPreview 
+            id="2"
+            :is-admin="isAdmin"
+            thumbnail="https://specials-images.forbesimg.com/imageserve/5f302109ffad89f9130e07db/960x0.jpg?cropX1=0&cropX2=4800&cropY1=243&cropY2=2943"
+            title="Hello there - the second time!"
+            previewText="This is my second post!"/>
+        <PostPreview 
+            id="3"
+            :is-admin="isAdmin"
+            thumbnail="https://specials-images.forbesimg.com/imageserve/5f302109ffad89f9130e07db/960x0.jpg?cropX1=0&cropX2=4800&cropY1=243&cropY2=2943"
+            title="Hi!"
+            previewText="This is my third post!"/>
+    </section>
+</template>
+
+<script>
+  import PostPreview from '@/components/Posts/PostPreview'
+
+  export default {
+    components:{
+      PostPreview
+    },
+    props: {
+      isAdmin: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style>
+    .post-list {
+        display: flex;
+        padding: 20px;
+        box-sizing: border-box;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+    import AdminPostForm from '@/components/Admin/AdminPostForm'
+    
+    export default {
+        components: {
+            AdminPostForm
+        }, data() {
+            return {
+                loadedPost: {
+                    author: 'Fernalia',
+                    title: 'My Awesome Post',
+                    thumbnailLink: 'https://specials-images.forbesimg.com/imageserve/5f302109ffad89f9130e07db/960x0.jpg?cropX1=0&cropX2=4800&cropY1=243&cropY2=2943',
+                    content: 'Super amazing, thanks for that!'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .update-form {
+        width: 90%;
+        margin: 20px auto;
+    }
+
+    @media (min-width: 768px) {
+        .update-form {
+            width: 500px;
+        }
+    }
+</style>
